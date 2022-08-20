39220a42fdc1a223d2db6f960b294c551e1590a6
Ek-Al <kathrin1ma@gmail.com>
2022-08-20 17:36:51 +0300

    fix week27

---
+++ week_27/tariff_app/src/components/App.js
@@ -0,0 +1,58 @@
+import React, {useState} from "react";
+import '../styles/App.css';
+import Card from './Card';
+
+const cards = [{
+  "id": "1",
+  "name": "Безлимитный 300",
+  "price": "300",
+  "mbit": "до 10 Мбит/сек",
+  "text": "Объем включенного трафика не ограничен",
+  "color": "rgb(99, 209, 245)",
+  isChecked: false,
+}, {
+  "id": "2",
+  "name": "Безлимитный 450",
+  "price": "450",
+  "mbit": "до 50 Мбит/сек",
+  "text": "Объем включенного трафика не ограничен",
+  "color": "rgb(53, 172, 152)",
+  isChecked: false,
+}, {
+  "id": "3",
+  "name": "Безлимитный 550",
+  "price": "550",
+  "mbit": "до 100 Мбит/сек",
+  "text": "Объем включенного трафика не ограничен",
+  "color": "rgb(219, 102, 78)",
+  isChecked: false,
+}, {
+  "id": "4",
+  "name": "Безлимитный 1000",
+  "price": "1000",
+  "mbit": "до 200 Мбит/сек",
+  "text": "Объем включенного трафика не ограничен",
+  "color": "rgb(68, 63, 63)",
+  isChecked: false,
+}];
+
+export default function App() {
+  const [currentData, setCurrentData] = useState(cards);
+
+const handleClick = (id) => {
+  setCurrentData(prevState => prevState.map(card => card.id === id ? ({ ...card, isChecked: true }) : ({ ...card, isChecked: false })))
+};
+
+
+    return ( 
+      <div className = "App">
+        <div className="cardsFlex">
+            {
+            currentData.map(card =>
+                <Card {...card} key = {card.id} onClick={handleClick}></Card>
+            )
+          }
+        </div>
+      </div>
+    );
+  }
---
+++ week_27/tariff_app/src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Card.css'
+
+export default (props) => {
+
+    const handleClick = () => props.onClick(props.id);
+
+    return (
+
+
+        < div onClick={handleClick} className={"tarifCard " + (props.isChecked ? "checked" : "")} >
+            <div className="cardName" style={{ backgroundColor: props.color }}>{props.name}</div>
+            <div className="cardPrice" style={{ backgroundColor: props.color }}><span className="rub">руб </span>{props.price}<span className="mes"> /мес</span></div>
+            <div className="cardMbit">{props.mbit}</div>
+            <div className="cardText">{props.text}</div>
+        </ div >
+    )
+}
